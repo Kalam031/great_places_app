1d5b94dca3ff7d3eeee8cce2dc60c830ba5e22d5
Kalam <Kalam@DESKTOP-2URI2JC>
2022-05-02 09:42:08 +0600

    adding add places screen and image input

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:great_places_app/Providers/great_places.dart';
 import 'package:great_places_app/screens/add_place_screen.dart';
+import 'package:great_places_app/screens/places_list_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -14,11 +15,33 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (ctx) => GreatPlaces(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          colorScheme: ColorScheme(
+            primary: Colors.blueAccent, // focus
+            onPrimary: Colors.white,
+
+            background: Colors.red,
+            onBackground: Colors.black,
+
+            secondary: Colors.orangeAccent, //float action button
+            onSecondary: Colors.white,
+
+            error: Colors.black,
+            onError: Colors.white,
+
+            surface: Colors.white, // AppBar
+            onSurface: Colors.black, //icons, inputs
+
+            brightness: Brightness.light,
+          ),
         ),
         home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
     );
   }
--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:great_places_app/widgets/image_input.dart';
 
-class PlacesListScreen extends StatelessWidget {
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titlecontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your places'),
-        actions: [
-          IconButton(
+        title: Text('Add a new place'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titlecontroller,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
             onPressed: () {},
             icon: Icon(Icons.add),
+            label: Text('Add Place'),
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              primary: Theme.of(context).colorScheme.secondary,
+            ),
           )
         ],
-      ),
-      body: Center(
-        child: CircularProgressIndicator(),
       ),
     );
   }
