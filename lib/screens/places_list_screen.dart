d246bcbeeecaf81777f816baf7ae8f6f53e1d433
Kalam <Kalam@DESKTOP-2URI2JC>
2022-05-06 11:25:00 +0600

    fetch and set place from database

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -18,22 +18,32 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: Text('No Place added. Start add some!'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.isEmpty
-            ? ch as Widget
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlaces.items[i].image),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<GreatPlaces>(
+                    child: Center(
+                      child: Text('No Place added. Start add some!'),
+                    ),
+                    builder: (ctx, greatPlaces, ch) => greatPlaces.items.isEmpty
+                        ? ch as Widget
+                        : ListView.builder(
+                            itemCount: greatPlaces.items.length,
+                            itemBuilder: (ctx, i) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    FileImage(greatPlaces.items[i].image),
+                              ),
+                              title: Text(greatPlaces.items[i].title),
+                              onTap: () {},
+                            ),
+                          ),
                   ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {},
-                ),
-              ),
       ),
     );
   }
